ba24942af84a7b19ea9a91cdfcc080a2b55cd204
Jaerry6700 <dmltkdltmdwo@gmail.com>
2025-12-25 19:22:00 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const cities = [
+  { name: "Paris", airport: "CDG" },
+  { name: "London", airport: "LHR" },
+  { name: "New York", airport: "JFK" },
+  { name: "Tokyo", airport: "HND" },
+  { name: "Seoul", airport: "ICN" },
+  { name: "Bangkok", airport: "BKK" },
+  { name: "Los Angeles", airport: "LAX" }
+];
